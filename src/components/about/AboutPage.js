7fbaf6cf31d7ceb4345d0c381276f177065b433d
bencallis1 <bencallis1@gmail.com>
2017-09-03 21:34:58 -0600

    added all the common components

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -3,9 +3,9 @@ import React from 'react';
 class AboutPage extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1>About page</h1>
-        <Link to="about" className="btn btn-primary btn-lg"/>
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
       </div>
     );
   }
